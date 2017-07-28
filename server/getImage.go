5de86837f9db0bd9f50c6700a600edd7c27f18b2
Shota Yamamota <1230124fw@gmail.com>
2017-07-29 02:17:50 +0900

    不要なif削除

--- server/getImage.go
+++ server/getImage.go
@@ -16,34 +16,32 @@ func getImageHeader(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", ", GET")
 	db := ConectDB()
 	defer db.Close()
-	if req.Method == http.MethodGet {
-		params := req.URL.Query()
-		lim, err := strconv.Atoi(params.Get("lim"))
-		if err != nil {
-			lim = 10
+	params := req.URL.Query()
+	lim, err := strconv.Atoi(params.Get("lim"))
+	if err != nil {
+		lim = 10
+	}
+	page, err := strconv.Atoi(params.Get("page"))
+	if err != nil {
+		page = 1
+	}
+	rows, err := db.Query("SELECT `id`, `lat`, `title`, `long`, `region`, `season`, `era`, `image`, `get_type`,`created_at`, `updated_at` FROM photos LIMIT ?,?;", (page-1)*lim, lim)
+	items := make([]Photo, lim)
+	if err != nil {
+		log.Printf("SELECT LIST ERRER:%v", err)
+	}
+	i := 0
+	for rows.Next() {
+		var t Photo
+		if err := rows.Scan(&t.ID, &t.Lat, &t.Title, &t.Long, &t.Region, &t.Season, &t.Era, &t.Image, &t.GetType, &t.CreateAt, &t.UpdatedAt); err != nil {
+			log.Fatal(err)
 		}
-		page, err := strconv.Atoi(params.Get("page"))
-		if err != nil {
-			page = 1
-		}
-		rows, err := db.Query("SELECT `id`, `lat`, `title`, `long`, `region`, `season`, `era`, `image`, `get_type`,`created_at`, `updated_at` FROM photos LIMIT ?,?;", (page-1)*lim, lim)
-		items := make([]Photo, lim)
-		if err != nil {
-			log.Printf("SELECT LIST ERRER:%v", err)
-		}
-		i := 0
-		for rows.Next() {
-			var t Photo
-			if err := rows.Scan(&t.ID, &t.Lat, &t.Title, &t.Long, &t.Region, &t.Season, &t.Era, &t.Image, &t.GetType, &t.CreateAt, &t.UpdatedAt); err != nil {
-				log.Fatal(err)
-			}
-			items[i] = t
-			i++
-		}
-		photos := Photos{Photos: items, Size: len(items)}
-		if err := json.NewEncoder(w).Encode(photos); err != nil {
-			panic(err)
-		}
+		items[i] = t
+		i++
+	}
+	photos := Photos{Photos: items, Size: len(items)}
+	if err := json.NewEncoder(w).Encode(photos); err != nil {
+		panic(err)
 	}
 
 }
--- server/imageUpload.go
+++ server/imageUpload.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	imageupload "github.com/olahol/go-imageupload"
@@ -15,42 +14,24 @@ func ImageSaveHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Access-Control-Allow-Credentials", "true")
 	rw.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-	rw.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	if req.Method == "OPTIONS" {
-		s := req.Header.Get("Access-Control-Request-Headers")
-		log.Println(s)
-		if strings.Contains(strings.ToLower(s), "authorization") {
-			rw.WriteHeader(http.StatusNoContent)
-			return
-		}
+	rw.Header().Set("Access-Control-Allow-Methods", "POST")
+	img, err := imageupload.Process(req, "file")
+	if err != nil {
+		log.Printf("Process :%s", err)
 		rw.WriteHeader(http.StatusNoContent)
 		return
 	}
-
-	if req.Method == http.MethodPost {
-		img, err := imageupload.Process(req, "file")
-		if err != nil {
-			log.Printf("Process :%s", err)
-			rw.WriteHeader(http.StatusNoContent)
-			return
-		}
-		thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
-		if err != nil {
-			log.Printf("ThumbanilPNG :%s", err)
-			rw.WriteHeader(http.StatusNoContent)
-			return
-		}
-		nowTime := time.Now().Unix()
-		err = thumb.Save(fmt.Sprintf("%d.png", nowTime))
-		if err != nil {
-			log.Printf("save :%s", err)
-			rw.WriteHeader(http.StatusNoContent)
-			return
-		}
-
+	thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
+	if err != nil {
+		log.Printf("ThumbanilPNG :%s", err)
+		rw.WriteHeader(http.StatusNoContent)
 		return
 	}
-	if req.Method != http.MethodPost {
+	nowTime := time.Now().Unix()
+	err = thumb.Save(fmt.Sprintf("%d.png", nowTime))
+	if err != nil {
+		log.Printf("save :%s", err)
+		rw.WriteHeader(http.StatusNoContent)
 		return
 	}
 }
