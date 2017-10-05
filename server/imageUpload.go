6a251f06d86db93fd3fd2949d4e2f15d9d8e0dea
Shota Yamamota <1230124fw@gmail.com>
2017-10-05 23:08:56 +0900

    aws s3にupload

--- server/imageUpload.go
+++ server/imageUpload.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	imageupload "github.com/olahol/go-imageupload"
 )
 
@@ -28,10 +33,48 @@ func ImageSaveHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 	nowTime := time.Now().Unix()
-	err = thumb.Save(fmt.Sprintf("%d.png", nowTime))
+	filename := fmt.Sprintf("%d.png", nowTime)
+	err = thumb.Save(filename)
+	uploadS3(filename)
 	if err != nil {
 		log.Printf("save :%s", err)
 		rw.WriteHeader(http.StatusNoContent)
 		return
 	}
+}
+
+func uploadS3(filename string) error {
+	// The session the S3 Uploader will use
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+	}))
+	// Create an uploader with the session and default options
+	uploader := s3manager.NewUploader(sess)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q, %v", filename, err)
+	}
+	defer f.Close()
+	fileInfo, _ := f.Stat()
+	size := fileInfo.Size()
+	buffer := make([]byte, size) // read file content to buffer
+
+	f.Read(buffer)
+	fileBytes := bytes.NewReader(buffer)
+	fileType := http.DetectContentType(buffer)
+
+	// Upload the file to S3.
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket:      aws.String("shop-bot-view"),
+		Key:         aws.String("aiphoto" + filename),
+		Body:        fileBytes,
+		ContentType: aws.String(fileType),
+		ACL:         aws.String("public-read-write"),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload file, %v", err)
+	}
+	fmt.Printf("file uploaded to, %s\n", result.Location)
+	return nil
 }
